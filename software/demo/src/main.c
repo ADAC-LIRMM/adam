698eef8eee307dc659fd46c907c3def347048e79
Raffael <raffel_rocha@hotmail.com>
2024-06-28 14:21:24 +0200

    Timer issue

--- software/demo/src/main.c
+++ software/demo/src/main.c
@@ -1,128 +1,101 @@
 #include "system.h"
-#define AUDIO_BUFFER_SIZE (64000)
-#define SAMPLE 0
-#define PRINT 1
-#define IDLE 2
 
 static void hw_init(void);
 void Spi_Setup();
+void Spi_Test();
 
-static int timer_flag = 0;
-uint32_t circularBuffer[AUDIO_BUFFER_SIZE];
-uint32_t circularBufferIndex = 0;
+int counter = 0;
 volatile int timer_interrupt_occurred = 0;
-bool print = 0;
+bool pin_state = false;
+static int timer_flag = 0;
 
-void timer_cb(void){
-    timer_flag = 1;
+void timer_cb(void) { timer_flag = 1; }
+
+void __attribute__((interrupt)) default_handler(void) {
+  timer_cb();
+  RAL.LSPA.TIMER[0]->ER = ~0;
+  timer_interrupt_occurred = 1;
 }
 
-void __attribute__((interrupt)) default_handler(void)
-{
-    timer_cb();
-    RAL.LSPA.TIMER[0]->ER = ~0;
-    timer_interrupt_occurred = 1;
+int main() {
+  hw_init();
+
+  set_timer_us(RAL.LSPA.TIMER[0], 5);
+  while (!timer_interrupt_occurred)
+    ; // Wait for the interrupt to occur
+
+  while (1) {
+    do {
+      _WFI();
+    } while (!timer_flag);
+    timer_flag = 0;
+    timer_interrupt_occurred = 0; // Reset the flag
+
+    pin_state = ~pin_state;
+    gpio_write(RAL.LSPA.GPIO[0], 0, pin_state);
+  }
+
+  return 0;
 }
 
-int main ()
-{
-    hw_init();
-    uart_init(RAL.LSPA.UART[0], 115200);
-    Spi_Setup();
+int main_lpu() { return 0; }
 
-    int state = SAMPLE;
+void hw_init(void) {
+  // Resume LPMEM
+  RAL.SYSCFG->LPMEM.MR = 1;
+  while (RAL.SYSCFG->LPMEM.MR)
+    ;
 
-    set_timer_16k(RAL.LSPA.TIMER[0]);
-    while(!timer_interrupt_occurred); // Wait for the interrupt to occur
+  // Resume MEMs
+  for (int i = 0; i < 3; i++) {
+    RAL.SYSCFG->MEM[i].MR = 1;
+    while (RAL.SYSCFG->MEM[i].MR)
+      ;
+  }
 
-    while(1) {
-        do {
-			_WFI();
-		} while(!timer_flag);
-		timer_flag = 0;
-        timer_interrupt_occurred = 0; // Reset the flag
-        switch(state){
-            case SAMPLE:
-                // Perform a read operation
-                uint32_t receivedData = SPI_ReceiveData();
-                // Store received Data in a circular buffer of 64KB
-                circularBuffer[circularBufferIndex] = receivedData;
-                circularBufferIndex = (circularBufferIndex + 1);
-                if (circularBufferIndex == AUDIO_BUFFER_SIZE) {
-                    state = PRINT;
-                }
-            break; 
-            case PRINT:
-                timer_interrupt_occurred = 0;
-                for(int i = 0; i < AUDIO_BUFFER_SIZE; i++){
-                    ee_printf("%d\r\n", circularBuffer[i]);
-                }
-                state = IDLE;
-            break;
-            case IDLE:
-                while(1);
-            break;
-        }
-    }
+  // Resume UART0
+  RAL.SYSCFG->LSPA.UART[0].MR = 1;
+  while (RAL.SYSCFG->LSPA.UART[0].MR)
+    ;
 
-    return 0;
+  // Resume TIMER0
+  RAL.SYSCFG->LSPA.TIMER[0].MR = 1;
+  while (RAL.SYSCFG->LSPA.TIMER[0].MR)
+    ;
+
+  // Resume GPIO0
+  RAL.SYSCFG->LSPA.GPIO[0].MR = 1;
+  while (RAL.SYSCFG->LSPA.GPIO[0].MR)
+    ;
+
+  // Resume SPI
+  RAL.SYSCFG->LSPA.SPI[0].MR = 1;
+  while (RAL.SYSCFG->LSPA.SPI[0].MR)
+    ;
+
+  // Enable LPU Interrupt
+  // RAL.SYSCFG->LPCPU.IER = ~0;
+
+  // Enable CPU Interrupt
+  RAL.SYSCFG->CPU[0].IER = ~0;
 }
 
-int main_lpu() 
-{
-    return 0;
+void Spi_Setup() {
+
+  // Set up SPI initialization parameters for temperature sensor (receiver)
+  ral_spi_t spiInit;
+  spiInit.PE = 1;   // Enable SPI peripheral
+  spiInit.TE = 0;   // Disable transmission
+  spiInit.RE = 1;   // Enable reception
+  spiInit.MS = 1;   // Set to master mode
+  spiInit.CPHA = 1; // Set clock phase to 1 - SPI Mode 3
+  spiInit.CPOL = 1; // Set clock polarity to 1 - SPI Mode 3
+  spiInit.DO = 1;   // Set data order to MSB first
+  spiInit.DL = 15;  // Set data length to 8 bits
+  spiInit.BRR = 50; // Main Clock = 50 MHz, SCLK between 1MHZ and 4 MHz
+
+  // Initialize SPI
+  SPI_Init(&spiInit);
 }
 
-void hw_init(void){
-    // Resume LPMEM
-    RAL.SYSCFG->LPMEM.MR = 1;
-    while(RAL.SYSCFG->LPMEM.MR);
-
-    // Resume MEMs
-    for (int i = 0; i < 3; i++) {
-        RAL.SYSCFG->MEM[i].MR = 1;
-        while(RAL.SYSCFG->MEM[i].MR);
-    }
-
-    // Resume UART0
-    RAL.SYSCFG->LSPA.UART[0].MR = 1;
-    while(RAL.SYSCFG->LSPA.UART[0].MR);
-
-    // Resume TIMER0
-    RAL.SYSCFG->LSPA.TIMER[0].MR = 1;
-    while(RAL.SYSCFG->LSPA.TIMER[0].MR);
-
-    // Resume GPIO0
-    RAL.SYSCFG->LSPA.GPIO[0].MR = 1;
-    while(RAL.SYSCFG->LSPA.GPIO[0].MR);
-        
-    // Resume SPI 
-    RAL.SYSCFG->LSPA.SPI[0].MR = 1;
-    while(RAL.SYSCFG->LSPA.SPI[0].MR);
-
-    // Enable LPU Interrupt
-    // RAL.SYSCFG->LPCPU.IER = ~0;
-
-    // Enable CPU Interrupt
-    RAL.SYSCFG->CPU[0].IER = ~0;
-}   
-
-void Spi_Setup(){
-
-    // Set up SPI initialization parameters for temperature sensor (receiver)
-    ral_spi_t spiInit;
-    spiInit.PE = 1;   // Enable SPI peripheral
-    spiInit.TE = 0;     // Disable transmission
-    spiInit.RE = 1;      // Enable reception
-    spiInit.MS = 1;         // Set to master mode
-    spiInit.CPHA = 1;         // Set clock phase to 1 - SPI Mode 3
-    spiInit.CPOL = 1;      // Set clock polarity to 1 - SPI Mode 3
-    spiInit.DO = 1;          // Set data order to MSB first
-    spiInit.DL = 15;         // Set data length to 8 bits
-    spiInit.BRR = 50;          // Main Clock = 50 MHz, SCLK between 1MHZ and 4 MHz
-    
-    // Initialize SPI
-    SPI_Init(&spiInit);
-}
-
-//cd ../../ ; make clean ; make all -j32 ; cd build/target ; riscv32-unknown-elf-gdb demo.elf 
+void Spi_Test() { delay_16K(RAL.LSPA.TIMER[0]); }
--- software/hal/src/timer.c
+++ software/hal/src/timer.c
@@ -4,105 +4,99 @@
 
 extern volatile int timer_interrupt_occurred;
 
-void timer_init(ral_timer_t  *timer, uint32_t presc, uint32_t val, uint32_t reload)
-{
-    timer->PR  = presc;
-    timer->VR  = val;
-    timer->ARR = reload;
-    timer->ER  = ~0;
-    timer->IER = ~0;
+void timer_init(ral_timer_t *timer, uint32_t presc, uint32_t val,
+                uint32_t reload) {
+  timer->PR = presc;
+  timer->VR = val;
+  timer->ARR = reload;
+  timer->ER = ~0;
+  timer->IER = ~0;
 }
 
-void set_timer_ms(ral_timer_t  *TIMERx, uint32_t ms)
-{
-    timer_stop(TIMERx);
-    TIMERx->PR = 24999;
-    TIMERx->VR = 0;
-    TIMERx->ARR = ms;
-    TIMERx->IER = 1;
-    timer_start(TIMERx);
+void set_timer_ms(ral_timer_t *TIMERx, uint32_t ms) {
+  timer_stop(TIMERx);
+  TIMERx->PR = 24999;
+  TIMERx->VR = 0;
+  TIMERx->ARR = ms;
+  TIMERx->IER = 1;
+  timer_start(TIMERx);
 }
 
-void set_timer_us(ral_timer_t  *TIMERx, uint32_t ms)
-{
-    timer_stop(TIMERx);
-    TIMERx->PR = 49;
-    TIMERx->VR = 0;
-    TIMERx->ARR = ms;
-    TIMERx->IER = 1;
-    timer_start(TIMERx);
+void set_timer_us(ral_timer_t *TIMERx, uint32_t us) {
+  timer_stop(TIMERx);
+  TIMERx->PR = 24;
+  TIMERx->VR = 0;
+  TIMERx->ARR = us;
+  TIMERx->IER = 1;
+  timer_start(TIMERx);
 }
 
-void set_timer_16k(ral_timer_t  *TIMERx)
-{
-    timer_stop(TIMERx);
-    TIMERx->PR = 1562;
-    TIMERx->VR = 0;
-    TIMERx->ARR = 1;
-    TIMERx->IER = 1;
-    timer_start(TIMERx);
+void set_timer_16k(ral_timer_t *TIMERx) {
+  timer_stop(TIMERx);
+  TIMERx->PR = 1562;
+  TIMERx->VR = 0;
+  TIMERx->ARR = 1;
+  TIMERx->IER = 1;
+  timer_start(TIMERx);
 }
 
-void timer_start(ral_timer_t  *timer)
-{
-    timer->PE = 1;
-    while(timer->PE != 1);
+void timer_start(ral_timer_t *timer) {
+  timer->PE = 1;
+  while (timer->PE != 1)
+    ;
 }
 
-void timer_stop(ral_timer_t  *timer)
-{
-    timer->PE = 0;
-    while(timer->PE != 0);
+void timer_stop(ral_timer_t *timer) {
+  timer->PE = 0;
+  while (timer->PE != 0)
+    ;
 }
 
-void delay_ms(ral_timer_t  *timer, uint32_t ms)
-{
-    timer->PE = 0;
-    timer->PR = 24999;
-    timer->VR = 0;
-    timer->ARR = ms;
-    timer->IER = ~0;
-    timer->PE = 1;
-    while(timer->PE != 1);
-    // Wait for timer to finish
-    while(!timer_interrupt_occurred); // Wait for the interrupt to occur
-    timer_interrupt_occurred = 0; // Reset the flag
+void delay_ms(ral_timer_t *timer, uint32_t ms) {
+  timer->PE = 0;
+  timer->PR = 24999;
+  timer->VR = 0;
+  timer->ARR = ms;
+  timer->IER = ~0;
+  timer->PE = 1;
+  while (timer->PE != 1)
+    ;
+  // Wait for timer to finish
+  while (!timer_interrupt_occurred)
+    ;                           // Wait for the interrupt to occur
+  timer_interrupt_occurred = 0; // Reset the flag
 }
 
-void delay_us(ral_timer_t  *timer, uint32_t us)
-{
-    timer->PE = 0;
-    timer->PR = 24;
-    timer->VR = 0;
-    timer->ARR = us;
-    timer->IER = ~0;
-    timer->PE = 1;
-    while(timer->PE != 1);
-    // Wait for timer to finish
-    while(!timer_interrupt_occurred); // Wait for the interrupt to occur
-    timer_interrupt_occurred = 0; // Reset the flag
+void delay_us(ral_timer_t *timer, uint32_t us) {
+  timer->PE = 0;
+  timer->PR = 24;
+  timer->VR = 0;
+  timer->ARR = us;
+  timer->IER = ~0;
+  timer->PE = 1;
+  while (timer->PE != 1)
+    ;
+  // Wait for timer to finish
+  while (!timer_interrupt_occurred)
+    ;                           // Wait for the interrupt to occur
+  timer_interrupt_occurred = 0; // Reset the flag
 }
 
-void delay_16K(ral_timer_t  *timer)
-{
-    timer->PE = 0;
-    timer->PR = 1399; // 1399
-    timer->VR = 0;
-    timer->ARR = 1;
-    timer->IER = ~0;
-    timer->PE = 1;
-    while(timer->PE != 1);
-    // Wait for timer to finish
-    while(!timer_interrupt_occurred); // Wait for the interrupt to occur
-    timer_interrupt_occurred = 0; // Reset the flag
+void delay_16K(ral_timer_t *timer) {
+  timer->PE = 0;
+  timer->PR = 1399; // 1399
+  timer->VR = 0;
+  timer->ARR = 1;
+  timer->IER = ~0;
+  timer->PE = 1;
+  while (timer->PE != 1)
+    ;
+  // Wait for timer to finish
+  while (!timer_interrupt_occurred)
+    ;                           // Wait for the interrupt to occur
+  timer_interrupt_occurred = 0; // Reset the flag
 }
 
-unsigned int get_timer_value(ral_timer_t  *timer)
-{
-    return timer->VR;
-}
+unsigned int get_timer_value(ral_timer_t *timer) { return timer->VR; }
 
-void timer_reset_value(ral_timer_t  *timer)
-{
-    timer->VR = 0;
-}
+void timer_reset_value(ral_timer_t *timer) { timer->VR = 0; }
